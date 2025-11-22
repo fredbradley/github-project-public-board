4511841efb6cff94f945b1bc50d01e7731cb5b6b
Fred Bradley <hello@fredbradley.co.uk>
2025-11-22 10:45:54 +0000

    add artisan command to make default user

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('make:default-user', function() {
+    User::query()->updateOrCreate([
+        'email' => 'admin@admin.com',
+    ],[
+        'password' => bcrypt('admin'),
+        'name' => 'Default Admin User'
+    ]);
+});
